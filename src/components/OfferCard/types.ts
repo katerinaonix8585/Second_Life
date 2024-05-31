ec8fb05efb5ebd72c9e130d4f31bbe8ef2d555a7
Alexandra376 <suvorovaalexandra344@gmail.com>
2024-05-31 19:07:13 +0300

    feat: add addition to offers page

--- src/components/OfferCard/types.ts
+++ src/components/OfferCard/types.ts
@@ -1,4 +1,13 @@
-export interface OfferCardProps {
+export interface OfferCardDataProps {
+  title: string;
   image: string;
   description: string;
+  location: string;
+  data: string;
+  typeOffer: string;
+  is_active: boolean;
+  is_free: boolean;
+  start_price: null | number;
+  step: null | number;
+  win_bid: null | number;
 }
