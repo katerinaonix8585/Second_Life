773db4450c2e2481be74c0c3e034af35dfd0eb61
katerinaonix8585 <katerinaonix8585@gmail.com>
2024-06-11 17:00:30 +0200

    fixDeploy

--- src/pages/CreateOffer/CreateOffer.tsx
+++ src/pages/CreateOffer/CreateOffer.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useFormik } from "formik";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Input from "components/Input/Input";
